88c937982a9f5147e8883cce6e26160223b2ad3e
Samy Dindane <samy@dindane.com>
2015-02-01 12:06:35 +0100

    Version bump (to 1.2.5)

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-java -jar futbin-watcher-1.2.4-jar-with-dependencies.jar --platform=pc --players-list=players_list.txt
+java -jar futbin-watcher-1.2.5-jar-with-dependencies.jar --platform=pc --players-list=players_list.txt

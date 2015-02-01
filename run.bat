ed03420c96998dcad1ad142c31a95c0d8e23994f
Samy Dindane <samy@dindane.com>
2015-02-01 22:00:14 +0100

    Fix the 10 seconds refresh delay bug

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-java -jar futbin-watcher-1.2.5-jar-with-dependencies.jar --platform=pc --players-list=players_list.txt
+java -jar futbin-watcher-1.2.6-jar-with-dependencies.jar --platform=pc --players-list=players_list.txt
